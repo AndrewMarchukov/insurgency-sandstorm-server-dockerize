4cbc2d2b74ac804ebe7220f61470740dd1d153a2
Andrew Marchukov <Jeklani@gmail.com>
2021-04-24 12:23:41 +0300

    Update restart-ins.sh

--- AutoUpdater/restart-ins.sh
+++ AutoUpdater/restart-ins.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-docker stop sandstorm-modmap
-docker rm sandstorm-modmap
-docker run -d --restart always --env-file /home/user/coop-modmap/modmap.env \
+REMOTEAPPVER=$(curl -s -X GET 'https://api.steamcmd.net/v1/info/581330' | jq -r -e '.data."581330".depots."581333".manifests.public')
+EXITSTATUS=$?
+if [ $EXITSTATUS -eq 0 ]; then
+  echo $REMOTEAPPVER > /opt/sandstorm-server.version
+  docker stop sandstorm-modmap
+  docker rm sandstorm-modmap
+  docker run -d --restart always --env-file /home/user/coop-modmap/modmap.env \
 --name sandstorm-modmap -p 12345:12345/udp -p 54321:54321/udp \
 -v /home/user/coop-modmap/Mods:/home/steam/steamcmd/sandstorm/Insurgency/Mods:rw \
 -v /home/user/coop-modmap/config/ini:/home/steam/steamcmd/sandstorm/Insurgency/Saved/Config/LinuxServer:ro \
 -v /home/user/coop-modmap/config/txt:/home/steam/steamcmd/sandstorm/Insurgency/Config/Server:ro andrewmhub/insurgency-sandstorm:latest
-REMOTEAPPVER=$(curl -s -X GET 'https://api.steamcmd.net/v1/info/581330' | jq -r -e '.data."581330".depots."581333".manifests.public')
-EXITSTATUS=$?
-if [ $EXITSTATUS -eq 0 ]; then
-echo $REMOTEAPPVER > /opt/sandstorm-server.version
 fi
